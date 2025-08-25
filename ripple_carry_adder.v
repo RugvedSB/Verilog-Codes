44194c2a593c8b9bd6333e4595b1451dd26acb4b
RugvedSB <rugvedsb2025@gmail.com>
2025-08-25 19:49:03 +0530

    ripple_carry_adder.v

--- ripple_carry_adder.v
+++ ripple_carry_adder.v
@@ -18,16 +18,18 @@
 
 
 module ripple_carry_adder(
-    output [3:0]S, cout, 
-    input [3:0]A, [3:0]B,
+    output [3:0]S,
+    output cout,
+    input [3:0]A,
+    input [3:0]B,
     input cin
     );
 
 wire c1, c2, c3;
 //Instantiating the full adders
-full_adder F1(S0, c1, A0, B0, cin);
-full_adder F2(S1, c2, A1, B1, c1);
-full_adder F3(S2, c3, A2, B2, c2);
-full_adder F4(S3, cout, A3, B3, c3);
+full_adder F1(A[0], B[0], cin, S[0], c1);
+full_adder F2(A[1], B[1], c1, S[1], c2);
+full_adder F3(A[2], B[2], c2, S[2], c3);
+full_adder F4(A[3], B[3], c3, S[3], cout);
 
 endmodule
